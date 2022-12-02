3b4a1f130a4d7c3920f320fe0457688b86815909
saltchess <saltrainchess@gmail.com>
2022-12-03 01:39:19 +0800

    fix: no Pinia

--- shulin/src/main.js
+++ shulin/src/main.js
@@ -8,9 +8,6 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 import axios from "axios"
-import { createPinia, PiniaVuePlugin } from 'pinia'
-Vue.use(PiniaVuePlugin)
-const pinia = createPinia()
 
 Vue.config.productionTip = true
 Vue.prototype.$axios = axios;
