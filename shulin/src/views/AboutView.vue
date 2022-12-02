f6fc3138692bb193e73c1eea32932586b9d659ce
jkm <20373758@buaa.edu.cn>
2022-12-02 12:53:29 +0800

    简单完成搜索框

--- shulin/src/views/AboutView.vue
+++ shulin/src/views/AboutView.vue
@@ -1,52 +1,121 @@
 <template>
   <div>
-    <div @click="open1">
-      成功
-    </div>
-    <el-button plain @click="open2">
-      警告
-    </el-button>
-    <el-button plain @click="open3">
-      消息
-    </el-button>
-    <el-button plain @click="open4">
-      错误
-    </el-button>
+    <el-row>
+      <el-col :span=6>
+        <div style="border: 1px solid transparent"></div>
+      </el-col>
+      <el-col :span="9">
+        <div class="search" :style="colorVar" @mouseout="changeColor" @mouseleave="changeColor2">
+          <el-row>
+            <el-col :span=20>
+              <div class="input_part">
+                <input type="text" class="input" v-model="searchInput" name="searchInput" />
+              </div>
+            </el-col>
+            <el-col :span=1>
+              <div class="icon_part">
+                <img src="@/assets/icon/clear.svg" class="clear_icon" v-if="(clearVisable === true)" @click="clear" />
+              </div>
+            </el-col>
+            <el-col :span=3>
+              <div class="icon_part">
+                <img src="@/assets/icon/search.svg" class="search_icon" />
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+
+    </el-row>
   </div>
 </template>
-
 <script>
 export default {
+  data() {
+    return {
+      color: 'rgb(192, 192, 192)',
+      clearVisable: false,
+      searchInput: ""
+    }
+  },
+  computed: {
+    colorVar() {
+      return {
+        '--color': this.color
+      }
+    }
+  },
   methods: {
-    open1() {
-      this.$notify({
-        title: '成功',
-        message: '这是一条成功的提示消息',
-        type: 'success'
-      });
+    changeColor() {
+      this.color = 'rgb(100, 100, 100)'
     },
-
-    open2() {
-      this.$notify({
-        title: '警告',
-        message: '这是一条警告的提示消息',
-        type: 'warning'
-      });
+    changeColor2() {
+      this.color = 'rgb(192, 192, 192)'
     },
-
-    open3() {
-      this.$notify.info({
-        title: '消息',
-        message: '这是一条消息的提示消息'
-      });
-    },
-
-    open4() {
-      this.$notify.error({
-        title: '错误',
-        message: '这是一条错误的提示消息'
-      });
+    clear() {
+      this.searchInput = ""
+    }
+  },
+  watch: {
+    "searchInput": {
+      handler: function () {
+        if (this.searchInput != "") {
+          this.clearVisable = true
+        }
+        else {
+          this.clearVisable = false
+        }
+      }
     }
   }
 }
 </script>
+<style scoped>
+.search {
+  border-radius: 20px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(214, 214, 214);
+  box-shadow: 0px 3px 6px var(--color);
+  transition: 0.3s;
+}
+
+.input_part {
+  padding: 15px;
+  border-radius: 20px;
+  border-color: black;
+  border-style: solid;
+  border-width: 0px;
+  border-right-width: 0px;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+}
+
+.icon_part {
+  border-radius: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-color: black;
+  border-style: solid;
+  border-width: 0px;
+}
+
+.clear_icon {
+  margin-top: 7px;
+  height: 18px;
+  cursor: pointer;
+}
+
+.search_icon {
+  height: 28px;
+  cursor: pointer;
+}
+
+.input {
+  height: 20px;
+  border: transparent;
+  outline: none;
+  width: 90%;
+  font-size: 17px;
+}
+</style>
